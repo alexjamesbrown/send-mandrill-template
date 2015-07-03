c958e126e2e09aef110ea0cc58907cf9b2792c10
Alex Brown <alex@alexjamesbrown.com>
2015-07-03 19:25:14 +0100

    initial commit - ported over from previous project

---
+++ index.js
@@ -0,0 +1,81 @@
+var _ = require('lodash'),
+    mandrill = require('mandrill-api/mandrill');
+
+function MandrillTemplateSender(apikey, defaults) {
+    if (!apikey)
+        throw new Error('Mandrill API key required');
+
+    if (defaults) {
+        this.defaults = defaults
+    } else {
+        this.defaults = {};
+    }
+
+    this.mandrill_client = new mandrill.Mandrill(apikey);
+}
+
+MandrillTemplateSender.prototype.sendTemplate = function(templateName, emailAddresses, variables, callback) {
+
+    if (typeof variables == 'function')
+        callback = variables;
+
+    var _emailAddresses = {};
+
+    if (typeof emailAddresses == 'string') {
+        _emailAddresses.to = emailAddresses
+    }
+
+    if (typeof emailAddresses == 'object') {
+        _emailAddresses = _.assign(this.defaults, emailAddresses)
+    }
+
+    if (!_emailAddresses.to)
+        return callback('No to address specified')
+
+    var message = {
+        to: [{
+            email: _emailAddresses.to,
+        }],
+
+        headers: {
+            'Reply-To': _emailAddresses.replyTo
+        },
+
+        track_opens: true,
+
+        merge: true,
+
+        merge_vars: [{
+            rcpt: _emailAddresses.to,
+            vars: []
+        }],
+    };
+
+    if (_emailAddresses.bcc) {
+        message.bcc_address = _emailAddresses.bcc
+    }
+
+    Object.keys(variables).forEach(function(variableKey) {
+        message.merge_vars[0].vars.push({
+            name: variableKey,
+            content: variables[variableKey]
+        })
+    })
+
+    this.mandrill_client.messages.sendTemplate({
+        template_name: templateName,
+        template_content: [],
+        message: message,
+    }, function(result) {
+        //log result console.log(result);
+        return callback();
+
+    }, function(e) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+        callback(e)
+    });
+}
+
+module.exports = MandrillTemplateSender
---
+++ test/test.js
@@ -0,0 +1,223 @@
+var should = require('chai').should(),
+    sinon = require('sinon'),
+    mandrill = require('mandrill-api/mandrill'),
+    proxyquire = require('proxyquire')
+
+describe('#sendTemplate', function() {
+
+    var sandbox;
+    var sendTemplateStub
+    var sendMandrillTemplate;
+
+    beforeEach(function() {
+        sandbox = sinon.sandbox.create();
+
+        sendTemplateStub = sandbox.stub().yields();
+
+        function MockMandrill(key) {
+            this.messages = {
+                sendTemplate: sendTemplateStub
+            }
+
+            this.apikey = key;
+        }
+
+        var defaults = {
+            replyTo: 'hello@domain.com'
+        };
+
+        sendMandrillTemplate = new(proxyquire('../', {
+            'mandrill-api/mandrill': {
+                Mandrill: MockMandrill
+            }
+        }))('testapikey', defaults);
+    })
+
+    afterEach(function() {
+        sandbox.restore();
+    })
+
+    it('mandrill client has passed in api key', function() {
+        sendMandrillTemplate.mandrill_client.apikey.should.equal('testapikey')
+    });
+
+    it('calls mandrill client with template name', function(done) {
+        sendMandrillTemplate.sendTemplate(
+            'template-name',
+            'a@b.com', {
+                'a': 1
+            },
+            function() {
+
+                sinon.assert.calledWith(sendTemplateStub, sinon.match({
+                    template_name: 'template-name'
+                }));
+
+                done();
+            })
+    });
+
+    it('calls mandrill client with template name', function(done) {
+        sendMandrillTemplate.sendTemplate(
+            'template-name',
+            'a@b.com', {
+                'a': 1
+            },
+            function() {
+
+                sinon.assert.calledWith(sendTemplateStub, sinon.match({
+                    template_name: 'template-name'
+                }));
+
+                done();
+            })
+    });
+
+    it('message contains to email address if specified as a string', function(done) {
+
+        sendMandrillTemplate.sendTemplate(
+            'template-name',
+            'a@b.com', {
+                'a': 1
+            },
+            function() {
+                sinon.assert.calledWith(sendTemplateStub, sinon.match({
+                    message: sinon.match({
+                        to: [{
+                            email: 'a@b.com'
+                        }]
+                    })
+                }));
+
+                done();
+            })
+    });
+
+    it('message contains to email address if specified in emailAddresses object', function(done) {
+
+        sendMandrillTemplate.sendTemplate(
+            'template-name', {
+                to: 'a@b.com'
+            }, {
+                'a': 1
+            },
+            function() {
+                sinon.assert.calledWith(sendTemplateStub, sinon.match({
+                    message: sinon.match({
+                        to: [{
+                            email: 'a@b.com'
+                        }]
+                    })
+                }));
+
+                done();
+            })
+    });
+
+    it('returns error if to address is not specified', function(done) {
+
+        sendMandrillTemplate.sendTemplate(
+            'template-name', {}, {
+                'a': 1
+            },
+            function(err) {
+                sinon.assert.notCalled(sendTemplateStub)
+                err.should.equal('No to address specified')
+                should.exist(err)
+
+                done();
+            })
+    });
+
+    it('has reply to address set by default', function(done) {
+        sendMandrillTemplate.sendTemplate(
+            'template-name', {
+                to: 'a@b.com'
+            }, {
+                'a': 1
+            },
+            function() {
+                sinon.assert.calledWith(sendTemplateStub, sinon.match({
+                    message: sinon.match({
+                        headers: {
+                            'Reply-To': 'hello@domain.com'
+                        }
+                    })
+                }));
+
+                done();
+            })
+    });
+
+    it('can specify reply-to address', function(done) {
+        sendMandrillTemplate.sendTemplate(
+            'template-name', {
+                to: 'a@b.com',
+                replyTo: '1@2.com'
+            },
+            function() {
+                sinon.assert.calledWith(sendTemplateStub, sinon.match({
+                    message: sinon.match({
+                        headers: {
+                            'Reply-To': '1@2.com'
+                        }
+                    })
+                }));
+
+                done();
+            })
+    });
+
+    it('can specify bcc address', function(done) {
+        sendMandrillTemplate.sendTemplate(
+            'template-name', {
+                to: 'a@b.com',
+                bcc: 'x@y.com'
+            },
+            function() {
+                sinon.assert.calledWith(sendTemplateStub, sinon.match({
+                    message: sinon.match({
+                        bcc_address: 'x@y.com'
+                    })
+                }));
+
+                done();
+            })
+    });
+
+    it('message contains merge_vars if specified', function(done) {
+        sendMandrillTemplate.sendTemplate(
+            'template-name',
+            'a@ba.com', {
+                a: 1,
+                abc: 'xyz'
+            },
+            function() {
+
+                //must be a better way of asserting
+                ////looked at sinon.match, but too recursive i think?
+
+                var vars = sendTemplateStub.args[0][0].message.merge_vars[0].vars;
+
+                should.exist(vars[0])
+                vars[0].name.should.equal('a')
+                vars[0].content.should.equal(1)
+
+                should.exist(vars[1])
+                vars[1].name.should.equal('abc')
+                vars[1].content.should.equal('xyz')
+
+                done();
+            })
+    });
+
+    it('do not have to specify merge_vars', function(done) {
+        sendMandrillTemplate.sendTemplate(
+            'template-name',
+            'a@ba.com',
+            function() {
+                sinon.assert.calledOnce(sendTemplateStub);
+                done();
+            })
+    });
+});
